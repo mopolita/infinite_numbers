d0840078dddb9c1f56037a3e9c89a93b8943181a
Alban Richard <alban.richard@etu.uca.fr>
2025-03-10 11:43:33 +0100

    some coding and declarations

--- src/infInt.cpp
+++ src/infInt.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
+#include <list>
+#include <compare>
+#include <iterator>
 #include "infInt.hpp"
 
+namespace inf{
+
+
+
+	auto operator<=>(const infInt& a, const infInt& b) {
+    	return std::lexicographical_compare_three_way(
+        	a.valeur.rbegin(), a.valeur.rend(),
+        	b.valeur.rbegin(), b.valeur.rend()
+    	);
+	}
+}
--- src/infInt.hpp
+++ src/infInt.hpp
@@ -5,16 +5,20 @@
 namespace inf{
 	class infInt{
 	private:
-		list<uint8_t> nb;
+		list<uint8_t> valeur;
 		bool positive;
 	public:
+		infInt() = default;
 		infInt(double nb);
+		// display func
 		friend std::ostream& operator<<(std::ostream &os, const infInt &nb);
 		// four basic operations
 		friend infInt operator+(const infInt &nb1, const infInt &nb2);
 		friend infInt operator-(const infInt &nb1, const infInt &nb2);
 		friend infInt operator*(const infInt &nb1, const infInt &nb2);
 		friend infInt operator/(const infInt &nb1, const infInt &nb2);
+		// comparison operators
+		friend auto operator<=>(const infInt& nb2, const infInt &nb1);
 	};
 }
 
